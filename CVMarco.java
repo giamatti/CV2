a1936647ddd9706d0856c6b3602de4d9948e3d5f
giamatti <seocialise1@gmail.com>
2016-04-22 17:53:13 +0200

    Added files via upload

---
+++ CVMarco.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cv.marco;
+
+/**
+ *
+ * @author Marco
+ */
+public class CVMarco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        mainscreen myscreen = new mainscreen();
+    }
+    
+}
